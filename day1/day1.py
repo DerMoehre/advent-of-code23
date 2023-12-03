c693dca442a03474a602fb6c748272390a129137
DerMoehre <94791958+DerMoehre@users.noreply.github.com>
2023-12-03 17:08:51 +0100

    addes solution for part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,17 +1,20 @@
-import pandas as pd
 
-test_input = [
-    "1abc2",
-    "pqr3stu8vwx",
-    "a1b2c3d4e5f",
-    "treb7uchet",
-]
+
 # read in data as txt file
 file = open("input.txt", "r")
 data = file.read()
 input = data.split('\n')
 
-def numbers(input):
+# --- PART ONE ---
+
+test_input_one = [
+    "1abc2",
+    "pqr3stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet",
+]
+
+def numbers_part_one(input):
     result = 0
     for number in input:
         digit = ''.join(x for x in number if x.isdigit())
@@ -25,4 +28,39 @@ def numbers(input):
             result += int(digit)
     return result
 
-print(numbers(input))
+print(numbers_part_one(input))
+
+# --- PART TWO ---
+
+test_input_two = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+]
+
+number_map = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e"
+}
+
+def string_to_int(input, mapping):
+    new_list = []
+    for word in input:
+        for key, item in mapping.items():
+            word = word.replace(key, str(item))
+        new_list.append(word)
+    return numbers_part_one(new_list)
+
+print(string_to_int(input, number_map))
+#print(translate)
